008abb7a0a848657ed29383d3b2c81f7771f975c
Yue Niu <niuyuesam@163.com>
2025-04-09 23:23:51 +0800

    add calibration plot

--- R/classif_model_compare.r
+++ R/classif_model_compare.r
@@ -30,12 +30,14 @@
 #' @examples
 #' data(cancer, package = "survival")
 #' cancer$dead <- factor(cancer$status == 2)
-#' cancer <- na.omit(cancer[, c("dead", "age", "ph.ecog")])
+#' cancer <- na.omit(cancer[, c("dead", "age", "sex", "meal.cal")])
 #'
-#' model <- glm(dead ~ age + ph.ecog, family = binomial(), data = cancer)
-#' cancer$logistic_pred <- predict(model, newdata = cancer, type = "response")
+#' model0 <- glm(dead ~ age, family = binomial(), data = cancer)
+#' cancer$age_pred <- predict(model0, type = "response")
+#' model <- glm(dead ~ age + sex + meal.cal, family = binomial(), data = cancer)
+#' cancer$logistic_pred <- predict(model, type = "response")
 #'
-#' classif_model_compare(cancer, "dead", c("logistic_pred", "age", "ph.ecog"))
+#' classif_model_compare(cancer, "dead", c("logistic_pred","age_pred"))
 classif_model_compare <- function(data, target_var, model_names, colors = NULL, output_files = TRUE,
                                   output_prefix = "model_compare", return_results = !output_files) {
   if (!output_files && !return_results) stop("Results are neither saved or returned!")
@@ -85,7 +87,7 @@ classif_model_compare <- function(data, target_var, model_names, colors = NULL, 
 
   plot_formula <- as.formula(paste0(target_var, " ~ ", paste(model_names, collapse = " + ")))
 
-  # Plot ROC curves
+  # Plot DCA curves
   vars_as_probability <- character()
   for (var in model_names) {
     if (any(data[[var]] < 0) || any(data[[var]] > 1)) {
@@ -124,4 +126,59 @@ classif_model_compare <- function(data, target_var, model_names, colors = NULL, 
   if (output_files) {
     ggplot2::ggsave(roc_plot, file = paste0(output_prefix, "_roc.png"), width = 4, height = 4)
   }
+
+  # plot calibration curves
+  n_tiles = min(round(nrow(data)/10),20)
+  data_calibration <- data %>%
+    pivot_longer(model_names)%>%
+    group_by(name) %>%
+    mutate(decile = ntile(value, n_tiles)) %>%
+    group_by(decile, name) %>%
+    summarise(
+      obsRate = mean(as.numeric(!!sym(target_var)), na.rm = T)-1,
+      predRate = mean(value, na.rm = T),
+      .groups = "drop"
+    ) %>%
+    as.data.frame()
+  data_calibration$name <- factor(data_calibration$name, levels = model_names)
+  calibration_plot <- ggplot(data = data_calibration, aes(
+    y = obsRate, x = predRate, group = name, color = name)) +
+    geom_smooth() +
+    lims(x = ifelse(rep(
+      is.null(x_lim),
+      2
+    ), c(min(data_calibration$predRate), max(data_calibration$predRate)),
+    x_lim
+    ), y = ifelse(rep(is.null(y_lim), 2), c(
+      min(data_calibration$obsRate_LCL),
+      max(data_calibration$obsRate_UCL)
+    ), y_lim)) +
+    geom_errorbar(aes(
+      ymax = .data$obsRate_UCL,
+      ymin = .data$obsRate_LCL
+    )) +
+    geom_abline(
+      intercept = 0,
+      slope = 1
+    ) +
+    scale_color_manual(values = ifelse(rep(
+      is.null(points_col_list),
+      n_groups
+    ), (ggplot2::scale_colour_brewer(palette = "Set3")$palette(8)[c(
+      4:8,
+      1:3
+    )])[c(1:n_groups)], points_col_list)) +
+    labs(
+      x = ifelse(is.null(xlab),
+        pred, xlab
+      ), y = ifelse(is.null(ylab), obs, ylab),
+      title = title
+    ) +
+    theme(
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(), panel.background = element_blank(),
+      axis.line = element_line(colour = "black"), legend.key = element_rect(fill = "white"),
+      axis.text = element_text(colour = "black", size = 12),
+      legend.position = legendPosition
+    )
 }
