67f60799f5c186bf077fa55224f4c7c856123ba8
Yue Niu <niuyuesam@163.com>
2025-04-09 17:28:34 +0800

    update classif_model_compare

--- R/classif_model_compare.r
+++ R/classif_model_compare.r
@@ -4,7 +4,10 @@
 #' @param data A data frame containing the target variable and the predicted values.
 #' @param target_var A string specifying the name of the target variable in the data frame.
 #' @param model_names A vector of strings specifying the names of the models to compare.
-#' @param filename A string specifying the filename to save the comparison results.
+#' @param colors A vector of colors to use for the plots.
+#' @param output_files A logical value indicating whether to output the results to files.
+#' @param output_prefix A string specifying the prefix for the output files.
+#' @param return_results A logical value indicating whether to return the results.
 #' @details This function calculates various performance metrics for each model
 #'   and output the comparison table and plots.
 #' @section Metrics:
@@ -32,15 +35,19 @@
 #' model <- glm(dead ~ age + ph.ecog, family = binomial(), data = cancer)
 #' cancer$logistic_pred <- predict(model, newdata = cancer, type = "response")
 #'
-#' classif_model_compare(cancer, "dead", c("logistic_pred","age", "ph.ecog"))
-classif_model_compare <- function(data, target_var, model_names, filename = "model_compare.csv") {
-  target <- factor(data[[target_var]])
-  model_val_comp <- data.frame(matrix(NA, nrow = length(model_names), ncol = 13))
-  colnames(model_val_comp) <- c(
+#' classif_model_compare(cancer, "dead", c("logistic_pred", "age", "ph.ecog"))
+classif_model_compare <- function(data, target_var, model_names, colors = NULL, output_files = TRUE,
+                                  output_prefix = "model_compare", return_results = !output_files) {
+  if (!output_files && !return_results) stop("Results are neither saved or returned!")
+  if (is.null(colors)) colors <- .color_panel
+  data[[target_var]] <- factor(data[[target_var]])
+  target <- data[[target_var]]
+  metric_table <- data.frame(matrix(NA, nrow = length(model_names), ncol = 13))
+  colnames(metric_table) <- c(
     "Model", "AUC", "Accuracy", "Sensitivity", "Specificity", "Pos Pred Value",
     "Neg Pred Value", "F1", "Kappa", "Brier", "cutoff", "Youden", "HosLem"
   )
-  model_val_comp$Model <- model_names
+  metric_table$Model <- model_names
   sens_metrics <- c(
     "Sensitivity", "Specificity", "Pos Pred Value",
     "Neg Pred Value", "F1"
@@ -49,8 +56,8 @@ classif_model_compare <- function(data, target_var, model_names, filename = "mod
   for (i in seq_along(model_names)) {
     model_name <- model_names[i]
     tmp <- pROC::coords(pROC::roc(target, data[[model_name]], direction = "<", quiet = TRUE), "best")
-    model_val_comp$cutoff[i] <- get_valid(tmp$threshold, mode = "last", disjoint = FALSE)
-    model_predict <- cut(data[[model_name]], c(-Inf, model_val_comp$cutoff[i], Inf), right = FALSE)
+    metric_table$cutoff[i] <- get_valid(tmp$threshold, mode = "last", disjoint = FALSE)
+    model_predict <- cut(data[[model_name]], c(-Inf, metric_table$cutoff[i], Inf), right = FALSE)
     levels(model_predict) <- levels(target)
     cm <- caret::confusionMatrix(model_predict, target,
       mode = "everything",
@@ -58,20 +65,63 @@ classif_model_compare <- function(data, target_var, model_names, filename = "mod
     )
     aucs <- pROC::ci.auc(target, data[[model_name]], direction = "<", quiet = TRUE)
     aucs <- format(aucs, digits = 2, nsmall = 3)
-    model_val_comp$AUC[i] <- paste0(aucs[2], " (", aucs[1], ", ", aucs[3], ")")
+    metric_table$AUC[i] <- paste0(aucs[2], " (", aucs[1], ", ", aucs[3], ")")
     for (j in seq_along(sens_metrics)) {
-      model_val_comp[i, sens_metrics[j]] <- cm$byClass[sens_metrics[j]]
+      metric_table[i, sens_metrics[j]] <- cm$byClass[sens_metrics[j]]
     }
     for (j in seq_along(acc_metrics)) {
-      model_val_comp[i, acc_metrics[j]] <- cm$overall[acc_metrics[j]]
+      metric_table[i, acc_metrics[j]] <- cm$overall[acc_metrics[j]]
     }
-    model_val_comp$Brier[i] <- DescTools::BrierScore(as.numeric(target) - 1, data[[model_name]])
-    model_val_comp$Youden[i] <- model_val_comp$Sensitivity[i] + model_val_comp$Specificity[i] - 1
-    model_val_comp$HosLem[i] <- hoslem.test(as.numeric(target) - 1, data[[model_name]])$p.value
+    metric_table$Brier[i] <- DescTools::BrierScore(as.numeric(target) - 1, data[[model_name]])
+    metric_table$Youden[i] <- metric_table$Sensitivity[i] + metric_table$Specificity[i] - 1
+    metric_table$HosLem[i] <- hoslem.test(as.numeric(target) - 1, data[[model_name]])$p.value
   }
-  for (i in 3:ncol(model_val_comp)) {
-    model_val_comp[, i] <- round(model_val_comp[, i], digits = 3)
+  for (i in 3:ncol(metric_table)) {
+    metric_table[, i] <- round(metric_table[, i], digits = 3)
   }
-  write.csv(model_val_comp, file = filename, row.names = FALSE)
-  model_val_comp
+  if (output_files) {
+    write.csv(metric_table, file = paste0(output_prefix, "_table.csv"), row.names = FALSE)
+  }
+
+  plot_formula <- as.formula(paste0(target_var, " ~ ", paste(model_names, collapse = " + ")))
+
+  # Plot ROC curves
+  vars_as_probability <- character()
+  for (var in model_names) {
+    if (any(data[[var]] < 0) || any(data[[var]] > 1)) {
+      vars_as_probability <- union(vars_as_probability, var)
+    }
+  }
+  dca_plot <- dcurves::dca(plot_formula, data = data, as_probability = vars_as_probability) %>%
+    plot(smooth = TRUE) +
+    ggplot2::scale_color_manual(values = colors) +
+    ggplot2::labs(x = "Threshold probability") +
+    ggplot2::theme_classic() +
+    ggplot2::theme(
+      axis.text = element_text(size = 12),
+      axis.title = element_text(size = 15)
+    )
+  if (output_files) {
+    ggplot2::ggsave(dca_plot, file = paste0(output_prefix, "_dca.png"), width = 5, height = 4)
+  }
+
+  # plot ROC curves
+  roc_list <- pROC::roc(plot_formula, data = data, direction = "<", quiet = TRUE)
+  names(roc_list) <- paste0(names(roc_list), " (", sapply(roc_list, function(x) sprintf("%.3f", x$auc)), ")")
+  roc_plot <- pROC::ggroc(roc_list, legacy.axes = TRUE) +
+    ggplot2::scale_color_manual(values = colors) +
+    ggplot2::geom_abline(slope = 1, intercept = 0, linetype = 2, alpha = 0.5) +
+    ggplot2::theme_classic() +
+    ggplot2::labs(x = "1 - Specificity", y = "Sensitivity") +
+    ggplot2::theme(
+      axis.text = element_text(size = 12),
+      axis.title = element_text(size = 15),
+      legend.title = element_blank(),
+      legend.text = element_text(size = 10),
+      legend.position = "inside",
+      legend.position.inside = c(0.7, 0.25),
+    )
+  if (output_files) {
+    ggplot2::ggsave(roc_plot, file = paste0(output_prefix, "_roc.png"), width = 4, height = 4)
+  }
 }
