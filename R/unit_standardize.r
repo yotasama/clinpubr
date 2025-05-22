2b569e6966d6433dbc3f934149c20aec7df8149b
Yue Niu <niuyuesam@163.com>
2025-05-22 17:08:06 +0800

    update unit_standardize

--- R/unit_standardize.r
+++ R/unit_standardize.r
@@ -4,7 +4,20 @@
 #' @param subject_col The name of the subject column.
 #' @param value_col The name of the value column.
 #' @param unit_col The name of the unit column.
-#' @param change_list A list of lists, each list contains the following elements:
+#' @param change_rules A data frame or a list of lists. If a data frame, it must contain the following columns:
+#'   \itemize{
+#'     \item `subject`: The subject to be standardized.
+#'     \item `unit`: The units of the subject.
+#'     \item `label`: The role of the unit, the rule is as follows:
+#'       \itemize{
+#'         \item `"t"`: the target unit to be standardized to. If not specified,
+#'           the function will use the most common unit in the data (retrieved by `first_mode()`).
+#'         \item `"r"`: The units to be removed, and the corresponding values be set to `NA`.
+#'           Set this when data with this unit cannot be used.
+#'         \item A number: Set the multiplier of this unit, the standardized value will be `value * multiplier`.
+#'           And `NA` and `""` is considered the same as `1`.
+#'   }
+#'   If a list of lists, each list contains the following elements:
 #'   \itemize{
 #'     \item `subject`: The subject to be standardized.
 #'     \item `target_unit`: The target unit to be standardized to. If not specified,
@@ -13,6 +26,8 @@
 #'       all units except the target unit. Must be specified to apply different `coeffs`.
 #'     \item `coeffs`: The coefficients to be used for the conversion. If not specified, the
 #'       function will use 1 for all units to be changed.
+#'     \item `units2remove`: The units to be removed, and the corresponding values be set to `NA`.
+#'           Set this when data with this unit cannot be used.
 #'   }
 #'
 #' @returns A data frame with subject units standardized.
@@ -22,29 +37,46 @@
 #'   subject = c("a", "a", "b", "b", "b", "c", "c"), value = c(1, 2, 3, 4, 5, 6, 7),
 #'   unit = c(NA, "x", "x", "x", "y", "a", "b")
 #' )
-#' change_list <- list(
+#' change_rules <- list(
 #'   list(subject = "a", target_unit = "x", units2change = c(NA), coeffs = c(20)),
 #'   list(subject = "b"),
 #'   list(subject = "c", target_unit = "b")
 #' )
 #' unit_standardize(df,
 #'   subject_col = "subject", value_col = "value", unit_col = "unit",
-#'   change_list = change_list
+#'   change_rules = change_rules
 #' )
-unit_standardize <- function(df, subject_col, value_col, unit_col, change_list) {
-  for (i in seq_along(change_list)) {
-    flag <- df[, subject_col] %in% change_list[[i]]$subject
+unit_standardize <- function(df, subject_col, value_col, unit_col, change_rules) {
+  if (is.data.frame(change_rules)) {
+    change_rules <- change_rules[!is.na(change_rules$label), ]
+    subjects <- unique(df[[subject_col]])
+    change_rules <- lapply(seq_along(subjects), function(i) {
+      tmp <- filter(change_rules, subject == subjects[i])
+      target_unit <- tmp$unit[which(tmp$label %in% "t")]
+      coeffs <- suppressWarnings(as.numeric(tmp$label))
+      units2change <- c(tmp$unit[!is.na(coeffs)], tmp$unit[which(tmp$label %in% "r")])
+      coeffs <- c(coeffs[!is.na(coeffs)], rep(NA, sum(tmp$label %in% "r")))
+      list(
+        subject = subjects[i],
+        target_unit = target_unit,
+        units2change = units2change,
+        coeffs = coeffs
+      )
+    })
+  }
+  for (i in seq_along(change_rules)) {
+    flag <- df[, subject_col] %in% change_rules[[i]]$subject
     df[flag, c(value_col, unit_col)] <- .unit_standardize(df[flag, c(value_col, unit_col)],
-      target_unit = change_list[[i]]$target_unit,
-      units2change = change_list[[i]]$units2change,
-      coeffs = change_list[[i]]$coeffs
+      target_unit = change_rules[[i]]$target_unit,
+      units2change = change_rules[[i]]$units2change,
+      coeffs = change_rules[[i]]$coeffs
     )
   }
   return(df)
 }
 
 .unit_standardize <- function(df, target_unit = NULL, units2change = NULL, coeffs = NULL) {
-  if (is.null(target_unit)) {
+  if (length(target_unit) == 0) {
     if (length(unique(df[, 2])) > 1) {
       target_unit <- first_mode(df[, 2])
     } else {
@@ -54,13 +86,8 @@ unit_standardize <- function(df, subject_col, value_col, unit_col, change_list) 
   if (length(target_unit) > 1) {
     stop("too many targets!")
   }
-  if (is.null(units2change)) {
-    units2change <- setdiff(unique(df[, 2]), target_unit)
-  }
-  if (!is.null(coeffs) && length(units2change) != length(coeffs)) {
+  if (length(coeffs) > 0 && length(units2change) != length(coeffs)) {
     stop("`coeffs` should have the same length as `units2change`!")
-  } else if (is.null(coeffs)) {
-    coeffs <- rep(1, length(units2change))
   }
   for (i in seq_along(units2change)) {
     flag <- df[, 2] %in% units2change[i]
@@ -68,4 +95,51 @@ unit_standardize <- function(df, subject_col, value_col, unit_col, change_list) 
   }
   df[, 2] <- target_unit
   return(df)
+}
+
+#' Generate a table of conflicting units.
+#' @description Get a table of conflicting units for the clinical data, along with the some useful information,
+#'   this table could be labeled and used for unit standardization.
+#' @param df A data frame of medical records that contains test subject, value, and unit cols.
+#' @param subject_col The name of the subject column.
+#' @param value_col The name of the value column.
+#' @param unit_col The name of the unit column.
+#' @param quantiles A vector of quantiles to be shown in the table.
+#' @param save_table A logical value indicating whether to save the table to a csv file.
+#' @param filename The name of the csv file to be saved.
+#'
+#' @returns A data frame of conflicting units.
+#' @export
+#' @examples
+#' x <- data.frame(subject = sample(c("a", "b"), 1000, replace = T), value = runif(1000))
+#' x$unit <- NA
+#' x$unit[x$subject == "a"] <- sample(c("mg/L", "g/l", "g/L"), sum(x$subject == "a"), replace = T)
+#' x$value[x$subject == "a" & x$unit == "mg/L"] <- x$value[x$subject == "a" & x$unit == "mg/L"] * 1000
+#' x$unit[x$subject == "b"] <- sample(c(NA, "g", "mg"), sum(x$subject == "b"), replace = T)
+#' x$value[x$subject == "b" & x$unit %in% "mg"] <- x$value[x$subject == "b" & x$unit %in% "mg"] * 1000
+#' x$value[x$subject == "b" & is.na(x$unit)] <- x$value[x$subject == "b" & is.na(x$unit)] *
+#'   sample(c(1, 1000), size = sum(x$subject == "b" & is.na(x$unit)), replace = T)
+unit_view <- function(df, subject_col, value_col, unit_col, quantiles = c(0.025, 0.975), save_table = TRUE,
+                      filename = NULL) {
+  res <- df %>%
+    group_by(!!as.symbol(subject_col), !!as.symbol(unit_col)) %>%
+    reframe(
+      label = NA, count = n(), nvalid = sum(!is.na(!!as.symbol(value_col))),
+      mean = mean(!!as.symbol(value_col), na.rm = TRUE), sd = sd(!!as.symbol(value_col), na.rm = TRUE),
+      median = median(!!as.symbol(value_col), na.rm = TRUE),
+      data.frame(quant_val = quantile(!!as.symbol(value_col), quantiles, na.rm = TRUE), quant = quantiles)
+    ) %>%
+    pivot_wider(names_from = quant, values_from = quant_val, names_prefix = "q_") %>%
+    group_by(!!as.symbol(subject_col)) %>%
+    filter(n() > 1) %>%
+    as.data.frame()
+  if (save_table) {
+    if (is.null(filename)) {
+      filename <- "unit_view.csv"
+    }
+    write.csv(res, file = filename, na = "")
+    invisible(res)
+  } else {
+    return(res)
+  }
 }
--- my_test.r
+++ my_test.r
@@ -1,10 +1,60 @@
 load_all()
-set.seed(1)
-data(cancer, package = "survival")
-res <- regression_scan(cancer, y = "status", time = "time")
-dat=cancer
-dat$ph.ecog=as.factor(dat$ph.ecog)
-model_res <- regression_fit(
-  data = dat, y = "status", time = "time", predictor = "ph.ecog",
-  covs = NULL, rcs_knots = NULL, returned = "predictor_combined"
-)
+x=data.frame(subject=sample(c("a","b"),1000,replace = T),value=runif(1000))
+x$unit=NA
+x$unit[x$subject=='a']=sample(c("mg/L","g/l","g/L"),sum(x$subject=='a'),replace = T)
+x$value[x$subject=='a' & x$unit=="mg/L"]=x$value[x$subject=='a' & x$unit=="mg/L"]*1000
+x$unit[x$subject=='b']=sample(c(NA,"g","mg"),sum(x$subject=='b'),replace = T)
+x$value[x$subject=='b' & x$unit%in%"mg"]=x$value[x$subject=='b' & x$unit%in%"mg"]*1000
+x$value[x$subject=='b' & is.na(x$unit)]=x$value[x$subject=='b' & is.na(x$unit)]*
+  sample(c(1,1000),size=sum(x$subject=='b' & is.na(x$unit)),replace = T)
+
+df=x
+subject_col="subject"
+value_col="value"
+unit_col="unit"
+quantiles=c(0.025,0.975)
+unit_view <- function(df, subject_col, value_col, unit_col, quantiles = c(0.025, 0.975), save_table = TRUE,
+                      filename = NULL) {
+  res <- df %>%
+    group_by(!!as.symbol(subject_col), !!as.symbol(unit_col)) %>%
+    reframe(
+      label = NA, count = n(), nvalid = sum(!is.na(!!as.symbol(value_col))),
+      mean = mean(!!as.symbol(value_col), na.rm = TRUE), sd = sd(!!as.symbol(value_col), na.rm = TRUE),
+      median = median(!!as.symbol(value_col), na.rm = TRUE),
+      data.frame(quant_val = quantile(!!as.symbol(value_col), quantiles, na.rm = TRUE), quant = quantiles)
+    ) %>%
+    pivot_wider(names_from = quant, values_from = quant_val, names_prefix = "q_") %>%
+    group_by(!!as.symbol(subject_col)) %>%
+    filter(n() > 1) %>%
+    as.data.frame()
+  if (save_table) {
+    if (is.null(filename)) {
+      filename <- "unit_view.csv"
+    }
+    write.csv(res, file = filename, na = "")
+    invisible(res)
+  } else {
+    return(res)
+  }
+}
+y=unit_view(df=x, subject_col="subject", value_col="value", unit_col="unit",save_table = F)
+y$label=c('t',NA,1e-3,1000,NA,'r')
+
+z=unit_standardize(df=x, subject_col="subject", value_col="value", unit_col="unit",change_rules = y)
+tmp=unit_view(df=z, subject_col="subject", value_col="value", unit_col="unit",save_table = F)
+change_rules=y
+change_rules <- change_rules[!is.na(change_rules$label), ]
+subjects <- unique(df[[subject_col]])
+change_rules <- lapply(seq_along(subjects), function(i) {
+  tmp <- filter(change_rules, subject == subjects[i])
+  target_unit <- tmp$unit[which(tmp$label %in% "t")]
+  coeffs <- suppressWarnings(as.numeric(tmp$label))
+  units2change <- c(tmp$unit[!is.na(coeffs)], tmp$unit[which(tmp$label %in% "r")])
+  coeffs <- c(coeffs[!is.na(coeffs)], rep(NA, sum(tmp$label %in% "r")))
+  list(
+    subject = subjects[i],
+    target_unit = target_unit,
+    units2change = units2change,
+    coeffs = coeffs
+  )
+})
